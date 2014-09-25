cf3fae27d53b216f25c7a5446511f8016b657235
agius <andrew@atevans.com>
2014-09-25 13:29:19 -0700

    Basic structure WIP

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////
+////  This works pretty much like Rails assets
+////  Check out Rakefile for the details
+//////////////////////////////////////////////
+
+//= require fortitude.jquery.js
+//= require murder.coffee
+
+function name(){
+  puts 'aaron'
+}
